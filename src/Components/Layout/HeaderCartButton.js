33542d7dbaf12b5195641d88f68ffc73fae809d3
Waqas Liaqat <waqasliaqat4444@gmail.com>
2023-06-27 21:09:24 +0500

    Header Cart button added

---
+++ src/Components/Layout/HeaderCartButton.js
@@ -0,0 +1,20 @@
+import React, { Fragment } from "react";
+import Carticon from '../Cart/CartIcon';
+import classes from './HeaderCartButton.module.css';
+const HeaderCartButton = (props) => {
+
+    return (
+        <button className={classes.button}>
+            <span className={classes.icon} >
+                <Carticon />
+            </span>
+            <span> Cart </span>
+            <span className={classes.badge}>3</span>
+        </button>
+    );
+
+
+};
+
+
+export default HeaderCartButton;
