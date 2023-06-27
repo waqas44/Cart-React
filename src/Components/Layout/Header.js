f8238e9143b45f2987fddb41b7264754d93a211d
Waqas Liaqat <waqasliaqat4444@gmail.com>
2023-06-27 20:17:51 +0500

    Header Added and header working

--- src/Components/Layout/Header.js
+++ src/Components/Layout/Header.js
@@ -3,15 +3,15 @@ import classes from './Header.module.css';
 import imagemeal from '../../Assets/meals.jpg';
 const Header = (props) => {
 
-    return <React.Fragment>
+    return <Fragment>
         <header className={classes.header}>
             <h1> React Meals</h1>
             <button>Cart</button>
 
         </header>
-        <div><img src={imagemeal} /> </div>
+        <div className={classes['main-image']}><img src={imagemeal} /> </div>
 
-    </React.Fragment>
+    </Fragment>
 
 
 }
