e14b0218176379d5cbe1946f8876d95cc6bdf792
Waqas Liaqat <waqasliaqat4444@gmail.com>
2023-07-03 20:16:41 +0500

    Cart popup showing on click

--- src/Components/Layout/Header.js
+++ src/Components/Layout/Header.js
@@ -7,7 +7,7 @@ const Header = (props) => {
     return <Fragment>
         <header className={classes.header}>
             <h1> React Meals</h1>
-            <HeaderCartButton onClick={props.showCartHandler} />
+            <HeaderCartButton onClick={props.onShowCart} />
 
         </header>
         <div className={classes['main-image']}><img src={imagemeal} /> </div>
